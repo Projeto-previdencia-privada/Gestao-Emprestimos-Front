111e22e520501775e7bb6e8c78bcb5e324c0744e
Neve0 <nevenarcisista@gmail.com>
2024-06-04 15:40:37 -0300

    Mudança no CSS do card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -5,19 +5,17 @@ function Card({clientInfo}) {
     return (
         <div className={'card'}>
             <div className={'card-content'}>
-                <div className={'wrapper'}>
+                <div className={'card-header'}>
                     <h1>Crédito disponível</h1>
                     <h2>R$ { parseFloat(clientInfo['credito-disponivel']).toFixed(2) }</h2>
-                    <div className={'card-footer'}>
-                        <div>
-                            <h3>Renda total</h3>
-                            <p>R$ { parseFloat(clientInfo["renda-total"]).toFixed(2)}</p>
-                        </div>
-                        <div>
-                            <h3>Total de crédito</h3>
-                            <p>R$ { parseFloat(clientInfo["credito-total"]).toFixed(2) }</p>
-                        </div>
-                    </div>
+                </div>
+                <div className={'card-renda'}>
+                    <h3>Renda total</h3>
+                    <p>R$ { parseFloat(clientInfo["renda-total"]).toFixed(2)}</p>
+                </div>
+                <div className={'card-total'}>
+                    <h3>Total de crédito</h3>
+                    <p>R$ { parseFloat(clientInfo["credito-total"]).toFixed(2) }</p>
                 </div>
             </div>
         </div>
